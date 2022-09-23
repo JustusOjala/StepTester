11cbcb4a2c3583a50f27d871b3507c1bc16bde5c
Justus Ojala <justus.ojala@aalto.fi>
2022-09-23 09:32:06 +0300

    Implemented initial control behavior

--- code/main.c
+++ code/main.c
@@ -1,10 +1,47 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define DELAY 100
+#define DELAY 10
 
 int main(){
-	
+	DDRA = 0xFF;
+	DDRB = 0x3F;
+
+	PORTA = 0;
+	PORTB = 0;
+
+	uint i;
+	//Spin in one direction
+	PORTB |= 0x08;
+	//Go through all 8 step settings
+	for(i = 0; i < 8; i++){
+		PORTA = i;
+		PORTB &= 0xFF - ~i << 4;
+
+		//Spin 1000 steps
+		for(uint j = 0; j < 1000; j++){
+			delay(DELAY);
+			PORTB |= 0x04;
+			delay(DELAY);
+			PORTB &= 0xFB;
+		}
+	}
+
+	//Spin in the other direction
+	PORTB &= 0xF7;
+	//Go through all 8 step settings
+	for(i = 0; i < 8; i++){
+		PORTA = i + 0x08;
+		PORTB &= 0xFF - ~i << 4;
+
+		//Spin 1000 steps
+		for(uint j = 0; j < 1000; j++){
+			delay(DELAY);
+			PORTB |= 0x04;
+			delay(DELAY);
+			PORTB &= 0xFB;
+		}
+	}
 	
 	return 0
 }
